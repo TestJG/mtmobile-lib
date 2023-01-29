b52405f0ac49b777b427630219745dbf8dbf0461
JBerg <josep.bergay@gmail.com>
2023-01-29 19:57:53 +0100

    test: point jest config to proper tsconfig

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+import { defaultsESM } from "ts-jest/presets";
+
+const transform = defaultsESM.transform;
+
+// tslint:disable-next-line: forin
+for (const key in transform) {
+    const transformConfig = transform[key];
+    if (typeof transformConfig === "string") {
+        continue;
+    }
+    transformConfig[1].tsconfig = "tsconfig.spec.json";
+}
+
+const config: JestConfigWithTsJest = {
+    preset: "ts-jest/presets/default-esm",
+    ...defaultsESM,
+    transform,
+    testPathIgnorePatterns: ["/compiled/.*", "/src/.*"],
+    testRegex: "/spec/.*\\.(test|spec)\\.(ts|tsx|js)$",
+    modulePathIgnorePatterns: ["models"],
+    moduleFileExtensions: ["ts", "tsx", "js"],
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "/test/",
+    ],
+};
+
+export default config;
