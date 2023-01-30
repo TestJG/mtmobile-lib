2862d5f7c88a7d607016df1491defd4763bac745
JBerg <josep.bergay@gmail.com>
2023-01-30 21:25:26 +0100

    chore: use rollup and esbuild to create bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,25 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import sourceMaps from 'rollup-plugin-sourcemaps';
-const pkg = require('./package.json');
-const camelCase = require('lodash.camelcase');
+import { defineConfig } from 'rollup';
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
 
-const libraryName = 'index';
-
-export default {
-    input: `compiled/${libraryName}.js`,
-    output: [
-        {
-            file: pkg.main,
-            name: camelCase(libraryName),
-            format: 'umd'
-        },
-        {
-            file: pkg.module,
-            format: 'es'
-        }
-    ],
-    sourcemap: true,
-    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-    external: [],
-    plugins: [
-        // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-        commonjs(),
-        // Allow node_modules resolution, so you can use 'external' to control
-        // which external modules to include in the bundle
-        // https://github.com/rollup/rollup-plugin-node-resolve#usage
-        resolve(),
-
-        // Resolve source maps to the original source
-        sourceMaps()
-    ]
-};
+export default defineConfig([
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: 'dist/index.cjs',
+                format: 'cjs'
+            },
+            {
+                file: 'dist/index.mjs',
+                format: 'es'
+            }
+        ],
+        plugins: [esbuild({ tsconfig: 'tsconfig.lib.json' })]
+    },
+    {
+        input: './compiled/types/index.d.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        plugins: [dts({ tsconfig: 'tsconfig.lib.json' })]
+    }
+]);
