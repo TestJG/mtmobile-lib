7d2efe372f555b378d4cecf67494b43175c222ee
JBerg <josep.bergay@gmail.com>
2023-01-30 23:11:01 +0100

    chore: remove compiled folder

--- compiled/types/src/processes/errors.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class TransientError extends Error {
-    constructor(message?: string);
-}
--- compiled/types/src/processes/makeRunTask.d.ts
+++
@@ -1,6 +0,0 @@
-import { Observable } from 'rxjs';
-import { TaskItem } from './processor.interfaces';
-import { ObsLike } from '../utils/rxutils';
-export declare function makeRunTask(runners: {
-    [name: string]: (payload: any) => ObsLike;
-}): (task: TaskItem) => Observable<any>;
